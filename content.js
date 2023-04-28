675f3d7f5c0395fad48938ba26ab3a02f45445aa
molotovah <43416516+molotovah@users.noreply.github.com>
2023-04-28 21:00:40 +0200

    Update content.js

--- content.js
+++ content.js
@@ -10,9 +10,27 @@ document.getElementById("generate-reply-button").addEventListener("click", funct
   // Send a message to the background script with the email content
   chrome.runtime.sendMessage({content: emailContent}, function(response) {
     // Callback function that is called when the background script sends a response
+    if (chrome.runtime.lastError) {
+      // Handle error if the background script is not running
+      console.error(chrome.runtime.lastError);
+      return;
+    }
+
     if (response && response.reply) {
       // Copy the generated reply to the clipboard
-      navigator.clipboard.writeText(response.reply);
+      navigator.clipboard.writeText(response.reply)
+        .then(function() {
+          // Show success message to the user
+          var successMessage = document.createElement("div");
+          successMessage.innerText = "AI-generated reply copied to clipboard!";
+          successMessage.style.color = "green";
+          successMessage.style.fontWeight = "bold";
+          document.querySelector("div[class^='EditableTextField']").appendChild(successMessage);
+        })
+        .catch(function(error) {
+          // Handle error if clipboard write fails
+          console.error(error);
+        });
     }
   });
 });
