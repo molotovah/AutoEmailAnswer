127c58620e57907d78fc028e55a1afbc1cabdb4d
molotovah <43416516+molotovah@users.noreply.github.com>
2023-04-28 21:01:47 +0200

    Update background.js

--- background.js
+++ background.js
@@ -12,3 +12,38 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         sendResponse({reply: generatedReply});
       });
     });
+    // Return true to indicate that a response will be sent asynchronously
+    return true;
+  }
+});
+
+function generateReply(emailContent, apiKey, replyTemplate, callback) {
+  // Call the AI-powered reply generator API with the email content and configuration options
+  // Replace this placeholder function with your actual implementation of the AI-powered reply generator API
+  // Make sure to handle errors and call the callback function with the generated reply
+  // Example implementation:
+  fetch("https://api.example.com/generate_reply", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + apiKey
+    },
+    body: JSON.stringify({
+      emailContent: emailContent,
+      replyTemplate: replyTemplate
+    })
+  })
+  .then(function(response) {
+    if (!response.ok) {
+      throw new Error("API error: " + response.status + " " + response.statusText);
+    }
+    return response.json();
+  })
+  .then(function(data) {
+    callback(data.reply);
+  })
+  .catch(function(error) {
+    console.error(error);
+    callback(null);
+  });
+}
