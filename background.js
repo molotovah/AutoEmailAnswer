ac0df30bb0c591cbe96c125e663c3f832981cc7b
molotovah <43416516+molotovah@users.noreply.github.com>
2023-04-28 21:01:05 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
 // Listen for messages from the content script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-  // Generate the AI-powered reply using some imaginary API
-  var generatedReply = generateReply(request.content);
-
-  // Send the generated reply back to the content script
-  sendResponse({reply: generatedReply});
-});
-
-function generateReply(emailContent) {
-  // Some imaginary code that generates an AI-powered reply
-  return "This is an AI-generated reply to your email.";
-}
+  if (request && request.content) {
+    // Get the configuration options from storage
+    chrome.storage.sync.get({
+      apiKey: "",
+      replyTemplate: "Thank you for your email. We will respond as soon as possible."
+    }, function(config) {
+      // Call the AI-powered reply generator API with the email content and configuration options
+      generateReply(request.content, config.apiKey, config.replyTemplate, function(generatedReply) {
+        // Send the generated reply back to the content script
+        sendResponse({reply: generatedReply});
+      });
+    });
